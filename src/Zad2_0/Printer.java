a2f73128b20dcf4cbd4dc7ff01fc7bab3c9cd704
Piachsecki <k.r.piasecki2@gmail.com>
2023-03-05 23:31:06 +0100

    Adding files

---
+++ src/Zad2_0/Printer.java
@@ -0,0 +1,16 @@
+public class Printer {
+    public static void main(String[] args) {
+        print_even(5);
+    }
+
+
+    public static void print_even(int N){
+        System.out.printf("Numbers that are fitting the condition given: ");
+        for (int i = 0; i <= N; i++) {
+            if(i % 2 == 0){
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+
+}
